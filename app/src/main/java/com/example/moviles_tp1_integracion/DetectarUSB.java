eef00fa396645ba821bcc1f3c83d1cbd84dc230f
SimaSoporte <sima.soporte@gmail.com>
2021-03-15 00:54:42 -0300

    Se agregó el Intent para iniciar la activity que hace la llamada al 911

--- app/src/main/java/com/example/moviles_tp1_integracion/DetectarUSB.java
+++ app/src/main/java/com/example/moviles_tp1_integracion/DetectarUSB.java
@@ -3,11 +3,15 @@ package com.example.moviles_tp1_integracion;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class DetectarUSB extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context,"Se ha conectado el USB", Toast.LENGTH_LONG).show();
+        Toast.makeText(context,"Se ha conectado/desconectado el USB. Se está iniciando una llamada al 911.", Toast.LENGTH_LONG).show();
+        Intent i = new Intent(Intent.ACTION_CALL);
+        i.setData(Uri.parse("tel:" + "911"));
+        context.startActivity(i);
     }
 }
